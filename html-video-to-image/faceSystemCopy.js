1f2f142e20110e8d44cf68291b7776da2311bd6e
emilianosca <emiliano_sanchez@criptext.com>
2022-10-06 12:40:26 -0500

    model now fully working :)

--- html-video-to-image/faceSystemCopy.js
+++ html-video-to-image/faceSystemCopy.js
@@ -1,7 +1,6 @@
-
-
-$(document).ready(function(){
-                
+export function faceSystem(){
+              
+    console.log("faceSystem working ! :)");
     async function face(){
         
         const MODEL_URL = '/models'
@@ -11,7 +10,7 @@ $(document).ready(function(){
         await faceapi.loadFaceRecognitionModel(MODEL_URL)
         await faceapi.loadFaceExpressionModel(MODEL_URL)
 
-        const img= document.getElementById('originalImg')
+        const img= document.getElementById('photo')
         let faceDescriptions = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors().withFaceExpressions()
         const canvas = $('#reflay').get(0)
         faceapi.matchDimensions(canvas, img)
@@ -22,18 +21,23 @@ $(document).ready(function(){
         faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 
         
-        const labels = ['sergio-01', 'mike-01', 'emi-01']
+        const labels = ['sergio-01', 'mike-01', 'emi-01', 'viche-01']
 
         const labeledFaceDescriptors = await Promise.all(
+
             labels.map(async label => {
 
-                const imgUrl = `images/${label}.jpg`
+                const imgUrl = `/images/${label}.jpg`
                 const img = await faceapi.fetchImage(imgUrl)
                 
                 const faceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 
                 if (!faceDescription) {
+                console.log('no faces detected for ${label}')
                 throw new Error(`no faces detected for ${label}`)
+                }
+                else {
+                    console.log('face detected for' + label)
                 }
                 
                 const faceDescriptors = [faceDescription.descriptor]
@@ -41,26 +45,36 @@ $(document).ready(function(){
             })
         );
 
+
+        console.log("start recognition");
         const threshold = 0.6
         const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, threshold)
 
         const results = faceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+        console.log("results were " + results);
 
         results.forEach((bestMatch, i) => {
+            console.log("ENTERED HERe " + bestMatch.toString())
             const box = faceDescriptions[i].detection.box
             const text = bestMatch.toString()
 
             if(threshold > bestMatch.distance){ 
+                console.log("face detected ! :)" + bestMatch.toString());
                 const drawBox = new faceapi.draw.DrawBox(box, { label: text })
                 drawBox.draw(canvas)
             }
             else {
+                console.log("no face detected ! :(");
                 const drawBox = new faceapi.draw.DrawBox(box, { label: 'no match' })
                 drawBox.draw(canvas)
             }
         })
+        console.log("end recognition");
 
     }
     
     face()
-})
+}
+
+
+
---
+++ html-video-to-image/mainProcess.js
@@ -0,0 +1,7 @@
+import * as faceSystem from './faceSystemCopy.js';
+// run faceSystem every 2 seconds
+setInterval(faceSystem.faceSystem, 2000);
+
+
+
+
