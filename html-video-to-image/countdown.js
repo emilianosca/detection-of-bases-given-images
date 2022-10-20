7f7f196a262997cf987bcde12942961017b82ca1
Miguel Pedraza <mikeped469@gmail.com>
2022-10-20 04:29:55 -0500

    Added some decoration and pop up functionality improved

---
+++ html-video-to-image/countdown.js
@@ -0,0 +1,24 @@
+let startingSeconds = 30;
+
+const countDownSpan = document.getElementById('countdown');
+
+function countdown() {
+    if (startingSeconds == 0) {
+        myStopFunction();
+        openPopUpTimer();
+    } else {    
+        startingSeconds--;
+        if (startingSeconds <= 9) {
+            countDownSpan.innerText = "0" + startingSeconds;
+        } else {
+            countDownSpan.innerText = startingSeconds;
+        }        
+    }
+}
+
+function myStopFunction() {
+    clearTimeout(myInterval);
+}
+
+const myInterval = setInterval(countdown, 1000);
+
--- html-video-to-image/popup.js
+++ html-video-to-image/popup.js
@@ -1,10 +1,20 @@
 const popup = document.getElementById('popup');
+const popupTimer = document.getElementById('popup-timer');
 const closePopupBtn = document.getElementById('close-popup');
+const closePopupBtnTimer = document.getElementById('close-popup-timer');
 
 function openPopUp() {
     popup.style.display = "block";
 }
 
+function openPopUpTimer() {
+    popup.style.display = "block";
+}
+
 closePopupBtn.addEventListener('click', () => {
+    popup.style.display = "none";
+});
+
+closePopupBtnTimer.addEventListener('click', () => {
     popup.style.display = "none";
 });
